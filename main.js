b7923f80c0c1050758db0ec8e7407da3b9356162
Bryson Elliott <brysonelliott@Brysons-MacBook-Pro.local>
2021-09-15 16:17:59 -0600

    Done with all but 3-5 in last section

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-let button = document.querySelector("#button1");
+
+let button = document.querySelector("#button-1");
 
 function buttonClicked() {
     console.log("You clicked the Button")
+    axios.get("https://swapi.dev/api/planets/?search=Alderaan").then((res) => {
+    
+    console.log(res.data.results)
+})
 };
 
+
+
 button.addEventListener('click', buttonClicked);
+
